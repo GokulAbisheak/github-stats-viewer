5002f07251b1a2f30e1d83cae74d21c249339a9c
IT21375132 <gokulabisheak12@gmail.com>
2023-07-05 03:35:43 +0530

    Completed Repo Stats

--- src/src/components/index.jsx
+++ src/src/components/index.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import LanguageStats from './LanguageStats';
 import Header from './Header';
 import Profile from './Profile';
+import RepoStats from './RepoStats';
 
 const Components = () => {
     return (
         <>
             <Header />
-            <div className='grid-container-1'>
-                <div className='grid-item-full'>
+            <div className='flex'>
+                <div className='flex-item'>
                     <Profile />
                 </div>
             </div>
-            <div className='grid-container-2'>
-                <div className='grid-item'>
+            <div className='flex'>
+                <div className='flex-item'>
                     <LanguageStats />
+                </div>
+                <div className='flex-item'>
+                    <RepoStats />
                 </div>
             </div >
         </>
