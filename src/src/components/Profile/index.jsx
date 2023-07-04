f062fc6f2a2ca48b272fe02d7f9efe3fdbebb763
IT21375132 <gokulabisheak12@gmail.com>
2023-07-05 03:35:20 +0530

    Added Loading Animation

--- src/src/components/Profile/index.jsx
+++ src/src/components/Profile/index.jsx
@@ -35,24 +35,43 @@ const Profile = () => {
             <div className='padding-x-md'>
                 <div className='left'>
                     {userData ? (
-                        <>
-                            <div className='profile-div margin-right-xl'>
-                                <img src={userData.avatar_url} />
-                            </div>
-                            <div className='profile-text'>
-                                <h1>{userData.name}</h1>
-                                <p>{userData.login}</p>
-                                <br />
-                                <p className='italic'>{userData.bio}</p>
-                                <br />
-                                <div className='text-grey'>
-                                    <FontAwesomeIcon icon={faUsers} /> {` ${userData.followers} Followers | ${userData.following} Following`}
+                        userData.login ? (
+                            <>
+                                <div className='profile-div margin-right-xl'>
+                                    <img src={userData.avatar_url} />
                                 </div>
-                            </div>
+                                <div className='profile-text'>
+                                    <h1>{userData.name}</h1>
+                                    <p>{userData.login}</p>
+                                    <br />
+                                    <p className='italic'>{userData.bio}</p>
+                                    <br />
+                                    <div className='text-grey'>
+                                        <FontAwesomeIcon icon={faUsers} /> {` ${userData.followers} Followers | ${userData.following} Following`}
+                                    </div>
+                                </div>
 
+                            </>
+                        ) : (
+                            <>
+                                <div className='center' style={{ paddingBottom: '20px' }}>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" style={{ fill: "blue" }}>
+                                            <animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite" />
+                                        </path>
+                                    </svg>
+                                </div>
+                            </>
+                        )) : (
+                        <>
+                            <div className='center' style={{ paddingBottom: '20px' }}>
+                                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" style={{ fill: "blue" }}>
+                                        <animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite" />
+                                    </path>
+                                </svg>
+                            </div>
                         </>
-                    ) : (
-                        <div></div>
                     )}
                 </div>
             </div>
