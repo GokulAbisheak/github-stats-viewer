a29b5aa878d70787e69542083d77d674636e9584
IT21375132 <gokulabisheak12@gmail.com>
2023-07-05 00:26:26 +0530

    Completed Profile

--- src/src/components/LanguageStats/index.jsx
+++ src/src/components/LanguageStats/index.jsx
@@ -14,10 +14,16 @@ const LanguageStats = () => {
     const [sortedLanguages, setSortedLanguages] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
+    const accessToken = import.meta.env.VITE_ACCESS_TOKEN
+
     useEffect(() => {
         const fetchUserData = async () => {
             try {
-                const response = await fetch(`https://api.github.com/users/${userId}`);
+                const response = await fetch(`https://api.github.com/users/${userId}`, {
+                    headers: {
+                        Authorization: `token ${accessToken}`,
+                    }
+                });
                 const json = await response.json();
                 setUserData(json);
             } catch (error) {
@@ -31,13 +37,21 @@ const LanguageStats = () => {
     useEffect(() => {
         const fetchRepoData = async () => {
             try {
-                const response = await fetch(`https://api.github.com/users/${userId}/repos`);
+                const response = await fetch(`https://api.github.com/users/${userId}/repos`, {
+                    headers: {
+                        Authorization: `token ${accessToken}`,
+                    }
+                });
                 const json = await response.json();
                 setRepoData(json);
 
                 let language = [];
                 const fetchLanguageData = json.map(async (repo) => {
-                    const response = await fetch(repo.languages_url);
+                    const response = await fetch(repo.languages_url, {
+                        headers: {
+                            Authorization: `token ${accessToken}`,
+                        }
+                    });
                     const json = await response.json();
                     language.push(json)
                 })
@@ -96,25 +110,23 @@ const LanguageStats = () => {
     return (
         <>
             <div className='box'>
-                <div>
-                    {userData ? (
-                        userData.name ? (
-                            <h3>{`${userData.name}'s Most Used Languages`}</h3>
-                        ) : (
-                            <h3>Most Used Languages</h3>
-                        )
+                {userData ? (
+                    userData.name ? (
+                        <h3>{`${userData.name}'s Most Used Languages`}</h3>
                     ) : (
-                        <div className='center'>
-                            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
-                                    <animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite" />
-                                </path>
-                            </svg>
-                        </div>
-                    )}
-                </div>
+                        <h3>Most Used Languages</h3>
+                    )
+                ) : (
+                    <div className='center'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
+                                <animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite" />
+                            </path>
+                        </svg>
+                    </div>
+                )}
                 {isLoading ? (
-                    <div className='center' style={{paddingBottom: '20px'}}>
+                    <div className='center' style={{ paddingBottom: '20px' }}>
                         <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z" style={{ fill: "blue" }}>
                                 <animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite" />
