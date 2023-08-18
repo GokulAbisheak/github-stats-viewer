e050c00fc65e36237c6d6ce86795969f117b9f8d
IT21375132 <gokulabisheak12@gmail.com>
2023-08-18 12:30:51 +0530

    Improved UI

--- src/src/components/Home/index.jsx
+++ src/src/components/Home/index.jsx
@@ -14,8 +14,14 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="h-screen flex flex-col justify-center items-center">
-        <div className="border-2 border-blue-700 items-stretch text-2xl rounded">
+      <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-b to-blue-300 from-blue-100">
+        <div className="text-3xl font-semibold text-blue-700">
+          {`<Github Stats Viewer />`}
+        </div>
+        <div className="text-xl text-blue-700 mb-[100px]">
+          Enter Username to View Dynamic Stats
+        </div>
+        <div className="border-2 border-blue-700 items-stretch text-2xl rounded bg-white">
           <input
             type="text"
             className="p-4 outline-0"
